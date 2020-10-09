f0905c76fa6b92147b6db85f760111ffa0c7886b
xiaxia <kathyxxj@service-inf-drzs-vpn-080.ethz.ch>
2020-10-09 14:50:16 +0200

    test

--- tools/datasets/jpeg_to_tf_record.py
+++ tools/datasets/jpeg_to_tf_record.py
@@ -81,7 +81,7 @@ def _convert_to_example(filename, image_buffer, label_int, label_str, height,
               'image/channels': _int64_feature(channels),
               'image/class/label': _int64_feature(label_int +
                                                   1),  # model expects 1-based
-              'image/class/synset': _bytes_feature(label_str),
+              'image/class/synset': _bytes_feature(label_str.encode('utf-8')),
               'image/format': _bytes_feature(image_format),
               'image/filename': _bytes_feature(os.path.basename(filename)),
               'image/encoded': _bytes_feature(image_buffer)
